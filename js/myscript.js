39b6e3c9897cdad2e336ccc8d1aff923dbb548d9
Yamam kurdi <yamamkurdi0@gmail.com>
2018-10-06 15:12:34 +0200

    code2

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	for(var i= 0 ; i < Movies.length;i++) {
-		$("#row").append("<div class='col-lg-6 text-center'> <div id='all'><img id='left' src='"+Movies[i].img+"'><div id='right'><h3>"+Movies[i].name+"</h3><h4>"+Movies[i].description+"</h4><div id='amout'><button class='btn'>Like</button><span class='spn'>"+Movies[i].likes+"</span></div></div></div></div>")
+		$("#row").append("<div class='col-lg-6 col-xs-12 text-center'> <div id='all'><img id='left' src='"+Movies[i].img+"'><div id='right'><h3>"+Movies[i].name+"</h3><h4 id='disc'>"+Movies[i].description+"</h4><div id='amout'><button class='btn'>Like</button><span class='spn'>"+Movies[i].likes+"</span></div></div></div></div>")
 	}
 	for(let n= 0 ; n < Movies.length;n++) {
 	$(".btn:eq("+n+")").click(function(){
